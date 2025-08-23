dc0f1d09236ed786431e1d686ca16e2beb4327d8
Shubham Ranjan <gkr74226@gmail.com>
2025-08-24 01:34:10 +0530

    OpenAndCloseConnection

--- src/JDBCCodes/TotalAndAverageSalary.java
+++ src/JDBCCodes/TotalAndAverageSalary.java
@@ -7,20 +7,25 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class StandardWayToOpenAndCloseConnection {
+public class TotalAndAverageSalary {
     public static void main(String[] args) {
         Connection conn = null;
         try{
              conn = DriverManager.getConnection("jdbc:oracle:thin:@//Shubham-Rj:1521/xe","advjavabatch2","mystudents");
-            System.out.println("Connected to the DB");
             Statement st = conn.createStatement();
-            ResultSet rs = st.executeQuery("Select ename,sal from employeees where deptno=10");
+            ResultSet rs = st.executeQuery("Select ename,sal from employees");
+            double total=0,count=0;
             while(rs.next())
             {
                 String ename = rs.getString(1);
                 double sal = rs.getDouble(2);
                 System.out.println(ename+"\t"+sal);
+                total = total + sal ;
+                count++;
             }
+            System.out.println("Total Salary is:"+total);
+            System.out.println("Avg salary is:"+total/count);
+            
         }catch(SQLException ex){
             System.out.println("Exception in DB:"+ex.getMessage());
         }
