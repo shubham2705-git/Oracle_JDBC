7862d9cf2ac0688abb87f1b96d8c5563e71ad5e9
Shubham Ranjan <gkr74226@gmail.com>
2025-08-23 15:55:44 +0530

    OpenAndCloseConnection

---
+++ src/JDBCCodes/StandardWayToOpenAndCloseConnection.java
@@ -0,0 +1,38 @@
+
+package JDBCCodes;
+
+import java.sql.ResultSet;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class StandardWayToOpenAndCloseConnection {
+    public static void main(String[] args) {
+        Connection conn = null;
+        try{
+             conn = DriverManager.getConnection("jdbc:oracle:thin:@//Shubham-Rj:1521/xe","advjavabatch2","mystudents");
+            System.out.println("Connected to the DB");
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery("Select ename,sal from employees where deptno=10");
+            while(rs.next())
+            {
+                String ename = rs.getString(1);
+                double sal = rs.getDouble(2);
+                System.out.println(ename+"\t"+sal);
+            }
+        }catch(SQLException ex){
+            System.out.println("Exception in DB:"+ex.getMessage());
+        }
+        finally{
+            try{
+                if(conn!=null){
+                    conn.close();
+                System.out.println("Disconnect successfully to the DB");
+                }
+            }catch(SQLException ex){
+             System.out.println("Exception in closing the connection:"+ex.getMessage());  
+            }
+        }
+    }
+}
