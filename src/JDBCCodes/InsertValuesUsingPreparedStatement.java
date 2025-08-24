c45c1152d123580a61ccf1e5dd946502fb9961fe
Shubham Ranjan <gkr74226@gmail.com>
2025-08-24 21:17:48 +0530

    InsertValuesUsingPreparedStatement

---
+++ src/JDBCCodes/InsertValuesUsingPreparedStatement.java
@@ -0,0 +1,52 @@
+package JDBCCodes;
+
+import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
+
+public class InsertValuesUsingPreparedStatement {
+    public static void main(String[] args) {
+        Connection conn = null;
+        Scanner kb = new Scanner(System.in);
+        System.out.println("Enter emp id");
+        int id = kb.nextInt();
+        
+        System.out.println("employee name");
+        String name =  kb.next();
+        
+        System.out.println("Enter Salary");
+        double amt = kb.nextInt();
+        
+        System.out.println("Enter depno");
+        int dno = kb.nextInt();
+        
+        try{
+             conn = DriverManager.getConnection("jdbc:oracle:thin:@//Shubham-Rj:1521/xe","advjavabatch2","mystudents");
+            PreparedStatement ps = conn.prepareStatement("Insert into employees values(?,?,?,?)");
+            ps.setInt(1, id);
+            ps.setString(2, name);
+            ps.setDouble(3, amt);
+            ps.setInt(4, dno);
+            int ans;
+            ans = ps.executeUpdate();
+            System.out.println("Record Inserted:"+ans);
+            
+            
+        }catch(SQLException ex){
+            System.out.println("Exception in DB:"+ex.getMessage());
+        }
+        finally{
+            try{
+                if(conn!=null){
+                    conn.close();
+                System.out.println("Disconnect successfully to the DB");
+                }
+            }catch(SQLException ex){
+             System.out.println("Exception in closing the connection:"+ex.getMessage());  
+            }
+       }
+    }
+}
